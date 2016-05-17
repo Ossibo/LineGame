329299a12edcd178def8688e95e09813a55f459d
Ossibo <card-gamer@hotmail.com>
2016-05-17 16:38:26 +0200

    Added LineGame project

---
+++ LineGame/Assets/Scripts/Board.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    //testing123
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //testing123
+	}
+}
