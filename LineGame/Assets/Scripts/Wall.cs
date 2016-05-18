6f930fad41eb7a4eff31dd5bc246392656d4f0e2
Ossibo <card-gamer@hotmail.com>
2016-05-18 15:20:07 +0200

    Added dymanic gameboard and a menu that loads levels

---
+++ LineGame/Assets/Scripts/Wall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : Tile {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
