a3d69078a90dd33f4871f91db3e8b2a18e38ca8b
Ossibo <card-gamer@hotmail.com>
2016-06-05 00:44:29 +0200

    Added code for right line texture

--- LineGame/Assets/Scripts/Floor.cs
+++ LineGame/Assets/Scripts/Floor.cs
@@ -62,7 +62,6 @@ public class Floor : Tile
     public Texture2D lineTexTurn;
 
     public bool hasLine;
-
     public Texture2D lineTexSelected;
     private Vector2 linePosition;
     private float lineRotation;
@@ -72,6 +71,11 @@ public class Floor : Tile
         linePosition = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x - 0.5f, -transform.position.y - 0.5f));
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFloor();
+    }
     // Select correct texture and set variables.
     void UpdateFloor()
     {
@@ -88,8 +92,31 @@ public class Floor : Tile
         }
         else if (parentTile && childTile)
         {
-            hasLine = true;
-            lineTexSelected = lineTexFull;
+
+            Vector2 parentDirection, childDirection, direction;
+
+            parentDirection = (parentTile.BoardIndex - BoardIndex);
+            childDirection = (childTile.BoardIndex - BoardIndex);
+
+            direction = parentDirection + childDirection;
+            if (direction.magnitude == 0)
+            {
+                lineTexSelected = lineTexFull;
+                Vector2 rotDirection = parentDirection - childDirection;
+                lineRotation = Vector2.Angle(rotDirection, new Vector2(1.0f, 0.0f));
+            }
+            else
+            {
+                lineTexSelected = lineTexTurn;
+                lineRotation = Vector2.Angle(direction, new Vector2(1.0f, 0.0f)) + -135;
+                if (direction.y >= 0)
+                {
+                    if (direction.x >= 0)
+                        lineRotation -= 90;
+                    else
+                        lineRotation += 90;
+                }
+            }
         }
         else
         {
